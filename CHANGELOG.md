19cfe70d7500f7ef1d85c59a4a30d6a55e439cd8
Nicholas Frawley <nfrawley93@gmail.com>
2019-10-02 18:23:00 -0500

    Update documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.1] - 2019.10.02
+
+* Update documentation and formatting
+
 ## [1.0.0] - 2019.10.02
 
 * Initial Release
--- lib/dart_geohash.dart
+++ lib/dart_geohash.dart
@@ -2,6 +2,7 @@ library dart_geohash;
 
 import 'dart:typed_data';
 
+/// A list of possible directions of neighboring geohash.
 enum Direction {
   NORTH,
   NORTHEAST,
@@ -14,9 +15,11 @@ enum Direction {
   CENTRAL
 }
 
+/// A class that can convert a geohash String to [Longitude, Latitude] and back.
 class GeoHasher {
   static String _baseSequence = '0123456789bcdefghjkmnpqrstuvwxyz';
 
+  /// Creates a Map of available characters for a geohash
   Map<String, int> _base32Map =
       Map.fromIterable(_baseSequence.split(""), key: (key) {
     return key;
@@ -24,6 +27,7 @@ class GeoHasher {
     return _baseSequence.indexOf(value);
   });
 
+  /// Creates a reversed Map of available characters for a geohash
   Map<int, String> _base32MapR =
       Map.fromIterable(_baseSequence.split(""), key: (key) {
     return _baseSequence.indexOf(key);
@@ -31,6 +35,7 @@ class GeoHasher {
     return value;
   });
 
+  /// Converts a List<int> of bits into a double for Longitude and Latitude
   double _bitsToDouble(
       {List<int> bits,
       double lower = -90.0,
@@ -47,6 +52,7 @@ class GeoHasher {
     return middle;
   }
 
+  /// Converts a double value Longitude or Latitude to a List<int> of bits
   List<int> _doubleToBits(
       {double value,
       double lower = -90.0,
@@ -69,6 +75,7 @@ class GeoHasher {
     return ret;
   }
 
+  /// Converts a List<int> bits into a String geohash
   String _bitsToGeoHash(List<int> bitValue) {
     List<int> remainingBits = List<int>.from(bitValue);
     List<int> subBits = List<int>();
@@ -91,6 +98,7 @@ class GeoHasher {
     return geoHashList.join("");
   }
 
+  /// Converts a String geohash into List<int> bits
   List<int> _geoHashToBits(String geohash) {
     List<int> letterNumValue = List<int>();
     List<int> bitList = List<int>();
@@ -115,6 +123,7 @@ class GeoHasher {
     return bitList;
   }
 
+  /// Encodes a given Longitude and Latitude into a String geohash
   String encode(double longitude, double latitude, {int precision = 12}) {
     bool precisionOdd = precision % 2 == 1;
     int originalPrecision = precision + 0;
@@ -149,6 +158,8 @@ class GeoHasher {
             : geohashString);
   }
 
+  /// Decodes a given String into a List<double> containing Longitude and
+  /// Latitude in decimal degrees.
   List<double> decode(String geohash) {
     if (geohash == null)
       throw ArgumentError.notNull(geohash);
@@ -176,6 +187,8 @@ class GeoHasher {
     ];
   }
 
+  /// Returns a String geohash of the neighbor of the given String in the given
+  /// direction.
   String _adjacent({String geohash, String direction}) {
     assert(direction.contains(new RegExp(r'[nsewNSEW]')),
         "Invalid Direction $direction not in NSEW");
@@ -219,6 +232,8 @@ class GeoHasher {
         _baseSequence[neighbor[direction][t].toString().indexOf(last)];
   }
 
+  /// Returns a Map<String, String> containing the `Direction` as the key and
+  /// the value being the geohash of the neighboring geohash in that direction.
   Map<String, String> neighbors(String geohash) {
     if (geohash == null)
       throw ArgumentError.notNull(geohash);
@@ -250,97 +265,98 @@ class GeoHasher {
   }
 }
 
+/// A containing class for a geohash
 class GeoHash {
   String _geohash;
   double _longitude;
   double _latitude;
   Map<String, String> _neighbors;
 
+  /// Returns the String geohash
   String get geohash {
     return _geohash;
   }
 
+  /// Returns the double longitude with an optional decimal accuracy
   double longitude({int decimalAccuracy}) {
-    if(decimalAccuracy == null)
+    if (decimalAccuracy == null)
       return _longitude;
-    else {
-
-    }
-      return double.parse(_longitude.toStringAsFixed(decimalAccuracy));
+    else {}
+    return double.parse(_longitude.toStringAsFixed(decimalAccuracy));
   }
 
+  /// Returns the double latitude with an optional decimal accuracy
   double latitude({int decimalAccuracy}) {
-    if(decimalAccuracy == null)
+    if (decimalAccuracy == null)
       return _latitude;
     else
       return double.parse(_latitude.toStringAsFixed(decimalAccuracy));
   }
 
+  /// Returns a Map<String, String> containing the `Direction` as the key and
+  /// the value being the geohash of the neighboring geohash in that direction.
   Map<String, String> get neighbors {
     return _neighbors;
   }
 
+  /// Returns a String geohash of a neighboring geohash in a given direction
   String neighbor(Direction direction) {
     return _neighbors[direction.toString().split(".")[1]];
   }
 
-  // Constructor for string geohash
-  GeoHash(String geohash){
+  /// Constructor given a String geohash
+  GeoHash(String geohash) {
     _geohash = geohash;
     _neighbors = GeoHasher().neighbors(geohash);
     _longitude = GeoHasher().decode(geohash)[0];
     _latitude = GeoHasher().decode(geohash)[1];
   }
 
-  // Constructor for decimal degrees
-  GeoHash.fromDecimalDegrees(double longitude, double latitude, {int precision = 9}) {
+  /// Constructor given Longitude and Latitude
+  GeoHash.fromDecimalDegrees(double longitude, double latitude,
+      {int precision = 9}) {
     this._longitude = longitude;
     this._latitude = latitude;
     _geohash = GeoHasher().encode(longitude, latitude, precision: precision);
     _neighbors = GeoHasher().neighbors(_geohash);
   }
 
-  // Requires both geohash to be at same precision
+  /// Returns true if given geohash is equal to or is a neighbor of this one.
   bool isNeighbor(String geohash) {
-
-    if(geohash.length != _geohash.length)
-      return false;
+    if (geohash.length != _geohash.length) return false;
 
     bool contains = false;
     _neighbors.forEach((key, value) {
-      if(value == geohash)
-        contains = true;
+      if (value == geohash) contains = true;
     });
 
     return contains;
   }
 
-  // Returns true if the given geohash contains this one within it.
+  /// Returns true if the given geohash contains this one within it.
   bool isInside(String geohash) {
-    if(geohash.length > _geohash.length)
-      return false;
+    if (geohash.length > _geohash.length) return false;
 
-    if(_geohash.substring(0, geohash.length) == geohash) {
+    if (_geohash.substring(0, geohash.length) == geohash) {
       return true;
     }
 
     return false;
   }
 
-  // Returns true if the given geohash is contained within this geohash
+  /// Returns true if the given geohash is contained within this geohash
   bool contains(String geohash) {
-    if(geohash.length < _geohash.length)
-      return false;
+    if (geohash.length < _geohash.length) return false;
 
-    if(geohash.substring(0, _geohash.length) == _geohash) {
+    if (geohash.substring(0, _geohash.length) == _geohash) {
       return true;
     }
 
     return false;
   }
 
-  // Returns a new Geohash for the parent of this one.
+  /// Returns a new Geohash for the parent of this one.
   GeoHash parent() {
-    return GeoHash(_geohash.substring(0,_geohash.length-1));
+    return GeoHash(_geohash.substring(0, _geohash.length - 1));
   }
 }
